6873419ce31f223d70636d932659b5e9fd694ebe
Isaac Hunter <ikehunter5@gmail.com>
2025-03-06 09:11:50 -0500

    add formatting, linting, and testing commands to client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,14 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest/config" />
+/// <reference types="vite/client" />
+
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+const __filename = import.meta.url.substring(
+  import.meta.url.lastIndexOf('/') + 1,
+)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +16,8 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+  },
+  resolve: {
+    alias: [{ find: 'src', replacement: path.resolve(__dirname, 'src') }],
   },
 })
