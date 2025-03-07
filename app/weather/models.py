99e9f46601820f818d839fc1978cc425acfa2a62
mat-toro11 <149001708+mat-toro11@users.noreply.github.com>
2025-03-06 21:55:08 -0500

    Practice Creating Model

--- app/weather/models.py
+++ app/weather/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from typing import ClassVar
+
+ 
+class WeatherData():
+    station_name = models.CharField(max_length=255, blank=False, null=False)
+    precip_inches = models.IntegerField(blank=False, null=False)
+    lat = models.FloatField(blank=False, null=False)
+    long = models.FloatField(blank=False, null=False)
 
 # Create your models here.
