0b1036ea85d9c3c2e6423f1c6ce18fdbf34a9b7c
mat-toro11 <149001708+mat-toro11@users.noreply.github.com>
2025-03-24 16:45:23 -0400

    DATABASE DATABASE DATABASE YIPPEE

--- app/weather/admin.py
+++ app/weather/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from weather.models import WeatherData
+
 # Register your models here.
+admin.site.register(WeatherData)
--- app/weather/models.py
+++ app/weather/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from typing import ClassVar
 
+from core.abstracts.models import ModelBase
+
  
-class WeatherData():
+class WeatherData(ModelBase):
     station_name = models.CharField(max_length=255, blank=False, null=False)
-    precip_inches = models.IntegerField(blank=False, null=False)
+    precip_inches = models.FloatField(blank=False, null=False)
     lat = models.FloatField(blank=False, null=False)
     long = models.FloatField(blank=False, null=False)
 
