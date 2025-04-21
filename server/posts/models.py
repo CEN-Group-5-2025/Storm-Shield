55d8dac513177c31823ddf4cbfd6bda7d8010c6c
mat-toro11 <149001708+mat-toro11@users.noreply.github.com>
2025-04-21 16:59:48 -0400

    Sorry Isaac

--- server/posts/models.py
+++ server/posts/models.py
@@ -1,3 +1,19 @@
+from asyncio.windows_events import NULL
+from pyexpat import model
+import time
 from django.db import models
+from django.template.defaultfilters import length
+from core.abstracts.models import ModelBase
 
 # Create your models here.
+class PostBase(ModelBase):
+    author = models.CharField()
+    title = models.CharField(null=False, max_length=20)
+    content = models.CharField(null=False, max_length=255)
+    votes = models.IntegerField()
+    '''POSSIBLY WORK ON USERVOTE'''
+    avatar = models.CharField(max_length=1)
+
+    @property
+    def timestamp(self):
+        return self.created_at
--- server/shelters/models.py
+++ server/shelters/models.py
@@ -5,9 +5,9 @@ from core.abstracts.models import ModelBase
 
 # Create your models here.
 class ShelterData(ModelBase):
-    shelter_name = models.CharField(max_length=255)
-    shelter_addy = models.CharField(max_length=255)
-    shelter_city = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    addy = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
     capacity = models.PositiveIntegerField()
     occupancy = models.PositiveIntegerField()
     lat = models.FloatField()
